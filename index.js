5ebf3ec967d135fae95813ce8031904ab6a6e7a9
AsumVictor <Iamasum369@gmail.com>
2023-09-05 20:00:15 +0000

    Fix linters

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ const navToggler = document.querySelector('.humbergur');
 const closeBtn = document.querySelector('.close-btn');
 const MobileNav = document.querySelector('.sticky-nav  .nav-links');
 const NavLinks = document.querySelectorAll('.nav-links li');
-const activeDeskTab = document.querySelector('.desk-tab.active');
-const activeMountainDesk = document.querySelector('.mountain.active-desk');
-
 
 mobileTabs.forEach((entry) => {
   entry.addEventListener('click', () => {
